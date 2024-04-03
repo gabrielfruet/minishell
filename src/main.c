8cf8db5a6a9dcc21ca51f0937b12ac5357674ada
gabrielfruet <gabrielfruet538@gmail.com>
2024-04-03 17:54:53 -0300

    feat: added print prompt

--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void print_prompt(void) {
+    const uint BUFFER_SIZE = 64;
+    char working_directory[BUFFER_SIZE];
+
+    const char* username = getlogin();
+    getcwd(working_directory, BUFFER_SIZE);
+    printf("%s at %s -> \n", username, working_directory);
+}
 
 int main(){
-    printf("Hello world");
+    print_prompt();
     return 1;
 }
