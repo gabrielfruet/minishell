c22a89321991ffd83c46ec5aa783d401ba2b055f
gabrielfruet <gabrielfruet538@gmail.com>
2024-04-08 12:45:26 -0300

    ...

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>h
+#include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <time.h>
@@ -8,7 +8,7 @@
 #include "command/redirection.h"
 #include "command/command.h"
 
-void print_prompt(int* elapse_time) {
+void print_prompt(void) {
     const uint BUFFER_SIZE = 64;
     char working_directory[BUFFER_SIZE];
 
@@ -17,16 +17,17 @@ void print_prompt(int* elapse_time) {
     char* home_name = getenv("HOME");
     char* home_start = strstr(working_directory, home_name);
     if (home_start != NULL) {
-        size_t home_size = strlen(home_name)*sizeof(char);
-        size_t cwd_size = strlen(working_directory)*sizeof(char);
+        size_t home_size = strlen(home_name)*sizeof(char); size_t cwd_size = strlen(working_directory)*sizeof(char);
         memmove(working_directory + 1, working_directory + home_size, BUFFER_SIZE-home_size);
         working_directory[0] = '~';
     }
     time_t current_time = time(NULL);
     struct tm* local_time = localtime(&current_time);
-    printf(BBLU "%s" WHT " at " YEL "%s\n" CRESET, username, working_directory);
+
     char hour_minute[6];
     strftime(hour_minute, sizeof(hour_minute)/sizeof(char), "%H:%M", local_time);
+
+    printf(BBLU "%s" WHT " at " YEL "%s\n" CRESET, username, working_directory);
     printf(BHYEL "%s" GRN " → " CRESET, hour_minute);
 }
 
@@ -46,7 +47,7 @@ char* read_input(char* buffer, int buffer_size) {
 
 int main(){
     while(1) {
-        print_prompt(NULL);
+        print_prompt();
         int buffer_size = 512;
         char user_input[buffer_size];
         if (NULL == read_input(user_input, buffer_size)) {
